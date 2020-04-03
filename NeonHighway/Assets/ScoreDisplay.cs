f40a78ba88d5cd74e5d4c5a6c23905d670fc6db6
ArminiusRey <glovett@masonlive.gmu.edu>
2020-04-03 09:48:12 -0400

    NEW  - Level end scene  - High Score sorting and display  - initials input system EDITS - N/A NOTES - all of the high score stuff is still in testing and not properly hooked up to the save data. I want to make sure it works with dummy data before introducing more complications.

---
+++ NeonHighway/Assets/ScoreDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+
+    public int displayNum;
+
+
+    public Text displayText;
+
+    
+    public List<SaveDataScript.ScoreData> scoreDataset;
+
+    public void Start()
+    {
+        scoreDataset.Sort(SortFunc);
+        
+        displayText.text = DisplayScores();
+    }
+
+    private int SortFunc(SaveDataScript.ScoreData a, SaveDataScript.ScoreData b)
+    {
+        if (a.scoreValue < b.scoreValue)
+        {
+            return 1;
+        }
+        else if (a.scoreValue > b.scoreValue)
+        {
+            return -1;
+        }
+        return 0;
+    }
+
+    public string DisplayScores()
+    {
+        string str = "";
+        for (int i = 0; i < displayNum; i++)
+        {
+            str += ("1): " + scoreDataset[i].scoreName + " : " + scoreDataset[i].scoreValue.ToString() + "\n");
+        }
+        return str;
+    }
+
+}
---
+++ NeonHighway/Assets/_NeonHighway/CharInput.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharInput : MonoBehaviour
+{
+
+    // ascii character are 65 - 90 for uppercase & 97 - 122 for lowercase
+    public Text CharDisplay;
+    public Text NameDisplay;
+    public int asciiValue = 65;
+
+    public bool cursorBlink;   
+    public float cursorBlinkSpeed;
+    public string myName;
+
+    private string cursorCharacter = "";
+    private bool cursorOn = false;
+    private float counter = 0;
+    private void Update()
+    {
+        CharDisplay.text = System.Convert.ToChar(asciiValue).ToString();
+        if (cursorBlink)
+        {
+            if (counter > cursorBlinkSpeed)
+            {
+                counter = 0;
+                if (!cursorOn)
+                {
+                    cursorOn = true;
+                    if (myName.Length < 3)
+                    {
+                        cursorCharacter = "_";
+                    }
+                }
+                else
+                {
+                    cursorOn = false;
+                    if (cursorCharacter.Length != 0)
+                    {
+                        cursorCharacter = cursorCharacter.Substring(0, cursorCharacter.Length - 1);
+                    }
+                }
+
+
+               
+            }
+            else
+            {
+                counter +=Time.deltaTime;
+            }          
+        }
+        NameDisplay.text = myName + cursorCharacter;
+    }
+
+    public void IncrementValue()
+    {
+        // clicked the upper button 
+        if (asciiValue < 90)
+        {
+            asciiValue++;
+        }
+        else
+        {
+            asciiValue = 65;
+        }
+    }
+    public void DecrementValue()
+    {
+        // clicked the lower button
+        if (asciiValue > 65)
+        {
+            asciiValue--;
+        }
+        else
+        {
+            asciiValue = 90;
+        }
+    }
+    public void SubmitChar()
+    {
+        // assign this character to the high score table name 
+        if (myName.Length < 3)
+        {
+                myName += System.Convert.ToChar(asciiValue).ToString();
+
+        }
+        else
+        {
+            // submit this name and score to the scoreboard and close the input menu.
+        }
+    }
+    public void CancelChar()
+    {
+        if (myName.Length > 0)
+        {
+                myName = myName.Substring(0, myName.Length - 1);
+        }
+
+    }
+}
